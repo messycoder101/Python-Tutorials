af6cf5a7aebd360d1d1473930094a58a6acb16ca
PRAKASH ANURAG, INI-DOS-TDE (EXT) <ANURAG.PRAKASH@swisscom.com>
2022-09-27 13:10:07 +0300

    Initial commit

---
+++ for Loops.py
@@ -0,0 +1,23 @@
+# Function to add all the numbers in the list
+prices = [10, 20, 30]
+total = 0
+for price in prices:
+    total += price # Shortcut for total = total + price
+print(total)
+
+''' Getting a list as an input from user
+# creating an empty list
+list = []
+
+# number of elements as input
+n = int(input("Enter number of elements : "))
+
+# iterating till the range
+for i in range(0, n):
+    element = int(input())
+
+	list.append(element) # adding the element
+	
+print(list)
+
+'''
