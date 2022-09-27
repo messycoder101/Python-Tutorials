a95b53c96def1a57af35b70b31f32778fd5f1540
PRAKASH ANURAG, INI-DOS-TDE (EXT) <ANURAG.PRAKASH@swisscom.com>
2022-09-27 09:36:30 +0300

    added program to print stars

---
+++ while_loops.py
@@ -0,0 +1,6 @@
+# Program to print stars using while loops
+i = 1               # initializing 'i'
+while i <=5:        # keep printing * until i = 5
+    print('*'*i)
+    i = i+1
+print("Done")       # print 'Done' when i = 5
